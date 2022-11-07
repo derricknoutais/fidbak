d9c14fdd7f3fec6faea7fd29c32d461ddb2c284d
Derrick Noutais <derricknoutais@gmail.com>
2022-11-07 18:03:00 +0100

    v2.9

--- app/Handle.php
+++ app/Handle.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Handle extends Model
 {
-    use HasFactory;
+    
 }
