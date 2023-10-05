8c17359ca7f707be7c652e1bf4f5ce353ba95402
derricknoutais <derricknoutais@gmail.com>
2023-10-05 14:43:40 +0100

    Subzero

--- app/Sub.php
+++ app/Sub.php
@@ -2,13 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sub extends Model
 {
     protected $guarded = [];
-    use HasFactory;
 
     public function produit()
     {
