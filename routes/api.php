c8ac42dfd43c172bb61644261742daab5ae0511d
derricknoutais <derricknoutais@gmail.com>
2023-12-13 08:41:01 +0100

    Add API Route for retrieving Articles

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  X-CSRF-TOKEN, X-Requested-With, Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/articles', function (Request $request) {
-    return 1;
+    return Article::all();
 });
