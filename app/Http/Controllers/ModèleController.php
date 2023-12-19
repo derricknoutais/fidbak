63c8f15a24bac43ef5e22860e19f6fa631d82b60
derricknoutais <derricknoutais@gmail.com>
2023-12-19 11:59:44 +0100

    Fixing Ajouter Modele

--- app/Http/Controllers/ModèleController.php
+++ app/Http/Controllers/ModèleController.php
@@ -10,35 +10,41 @@ use App\TypeMoteur;
 
 class ModèleController extends Controller
 {
-    public function all(){
+    public function all()
+    {
         return Modèle::orderBy('nom')->get();
     }
-    public function chercheModèle($type){
-        return Type::where('id', $type)->with('modèles')->first();
+    public function chercheModèle($type)
+    {
+        return Type::where('id', $type)
+            ->with('modèles')
+            ->first();
     }
-    public function enregistrer(Request $request){
-        $modèle = Modèle::create([
-            'nom' => $request->modèle,
-            'marque_id' => $request->marque['id']
-        ]);
-        if($request->moteur){
-            $moteur = Moteur::create([
-                'nom' => $request->moteur,
-                'marque_id' => $request->marque['id']
+    public function enregistrer(Request $request)
+    {
+        if ($request->modèle) {
+            $modèle = Modèle::create([
+                'nom' => $request->modèle,
+                'marque_id' => $request->marque['id'],
             ]);
         }
-        if($request->type && $modèle){
+        if ($request->moteur) {
+            $moteur = Moteur::create([
+                'nom' => $request->moteur,
+                'marque_id' => $request->marque['id'],
+            ]);
+        }
+        if ($request->type && $modèle) {
             ModèleType::create([
                 'modèle_id' => $modèle->id,
-                'type_id' => $request->type['id']
+                'type_id' => $request->type['id'],
             ]);
-            if($moteur){
-                TypeMoteur::create([
-                    'moteur_id' => $moteur->id,
-                    'type_id' => $request->type['id']
-                ]);
-            }
-
+        }
+        if ($request->type && $moteur) {
+            TypeMoteur::create([
+                'moteur_id' => $moteur->id,
+                'type_id' => $request->type['id'],
+            ]);
         }
     }
 }
